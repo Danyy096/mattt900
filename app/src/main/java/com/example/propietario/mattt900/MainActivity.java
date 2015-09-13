07256bc72b1b33c790b939ffe0ddbd0bc2ed7394
daniel reyes <daniel020996@gmail.com>
2015-09-13 07:49:35 -0500

    Diosesbueno

--- app/src/main/java/com/example/propietario/mattt900/MainActivity.java
+++ app/src/main/java/com/example/propietario/mattt900/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.propietario.mattt900;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView WebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView= (WebView) findViewById(R.id.webview)
+                WebView.loadUrl("http://matt900.esy.es/untitled%20folder/PROYECTO.html");
     }
 
     @Override
